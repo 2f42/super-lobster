5372d4f4f7cc5331fc350ce1542425e79af34cc2
baconsenpaii <baconforevery2@gmail.com>
2021-12-02 03:15:24 +0000

    added domains to lobster

--- superlobster/interpreter.py
+++ superlobster/interpreter.py
@@ -2,23 +2,25 @@ from __future__ import annotations
 
 import itertools
 
-from .geometry import Point, Direction
+from .geometry import Point, Direction, Domain
 
 
 class Lobster:
 
     id_iter = itertools.count()
 
-    def __init__(self, position=Point(), direction=Direction.EAST) -> None:
+    def __init__(self, position=Point(), direction=Direction.EAST, domain=Domain()) -> None:
         self.id = next(Lobster.id_iter)
         self.position = position
         self.direction = direction
+        self.domain = domain
 
     def __repr__(self) -> str:
-        return f"<Lobster {self.id} at {self.position} facing {self.direction.name}>"
+        return f"<Lobster {self.id} at {self.position} facing {self.direction.name} in {self.domain}>"
 
     def step(self, steps: int = 1) -> None:
-        self.position += Point.from_direction(self.direction) * steps
+        new_pos = self.position + Point.from_direction(self.direction) * steps
+        self.position = self.domain.transform(new_pos)
 
     def turn(self, steps: int) -> None:
         self.direction += steps
