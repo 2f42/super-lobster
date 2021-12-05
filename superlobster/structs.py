2268ba27cbabd4bff3033e53d082f145bfa14583
baconsenpaii <baconforevery2@gmail.com>
2021-12-05 02:53:42 +0000

    iterate over the matrix

--- superlobster/structs.py
+++ superlobster/structs.py
@@ -1,5 +1,4 @@
 from __future__ import annotations
-
 from typing import Dict
 
 class Matrix:
@@ -9,10 +8,21 @@ class Matrix:
 		self._height: int = height
 		self.__matrix: Dict[int, int] = {i: 0 for i in range(width * height)}
 
+	def __iter__(self) -> Matrix:
+		self.__i: int = 0
+		self.__max: int = self.width * self.height
+		return self
+
+	def __next__(self):
+		if self.__i < self.__max:
+			yield self.__matrix[self.__i]
+			self.__i += 1
+		else:
+			raise StopIteration
+
 	@property
 	def width(self) -> int:
 		return self._width
-
 
 	@property
 	def height(self) -> int:
